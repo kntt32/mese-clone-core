12c116b8cd510fd919a3c019557db2b2887979f0
knt.t <153593861+kntt32@users.noreply.github.com>
2024-11-19 01:07:37 +0000

    作業中

--- main.js
+++ main.js
@@ -1,9 +1,105 @@
+class Industry {
+    constructor(init, player_count) {
+        this.company = [];
+        for(let i=0; i<player_count; i++) {
+            this.company.push(new Company(init));
+            this.company[i].player_count = player_count;
+        }
+    }
+
+    next(decisions) {
+        if(this.company.length != decisions.length) {
+            alert("Err");
+            return undefined;
+        }
+        for(let i=0; i<this.company.length; i++) {
+            this.company[i].decision = decisions[i];
+        }
+        let sum_decision_mk = calc_sum_decision_mk(decisions);
+        let sum_decision_price = calc_sum_decision_price(decisions);
+        let sum_goods_max_sales = calc_sum_goods_max_sales(this.company);
+        let sum_goods = calc_sum_goods(this.company);
+        let sum_sales = calc_sum_sales(this.company);
+        let sum_sold = calc_sum_sold(this.company);
+        let sum_history_rd = calc_sum_history_rd(this.company);
+
+        for(let i=0; i<this.company.length; i++) {
+            this.company[i].sum_decision_mk = sum_decision_mk;
+            this.company[i].sum_decision_price = sum_decision_price;
+            this.company[i].average_mk = sum_decision_mk / this.company.length;
+            this.company[i].sum_goods_max_sales = sum_goods_max_sales;
+            this.company[i].sum_goods = sum_goods;
+            this.company[i].sum_sales = sum_sales;
+            this.company[i].sum_sold = sum_sold;
+            this.company[i].sum_history_rd = sum_history_rd;
+        }
+
+        function calc_sum_decision_mk(decisions) {
+            let sum = 0;
+            for(let i=0; i<decisions.length; i++) {
+                sum += decisions.mk;
+            }
+            return sum;
+        }
+
+        function calc_sum_decision_price(decisions) {
+            let sum = 0;
+            for(let i=0; i<decisions.length; i++) {
+                sum += decisions[i].price;
+            }
+            return sum;
+        }
+
+        function calc_sum_goods_max_sales(companies) {
+            let sum = 0;
+            for(let i=0; i<companies.length; i++) {
+                sum += companies[i].goods_max_sales;
+            }
+            return sum;
+        }
+
+        function calc_sum_goods(companies) {
+            let sum = 0;
+            for(let i=0; i<companies.length; i++) {
+                sum += companies[i].goods;
+            }
+            return sum;
+        }
+
+        function calc_sum_sales(companies) {
+            let sum = 0;
+            for(let i=0; i<companies.length; i++) {
+                sum += companies[i].sales;
+            }
+            return sum;
+        }
+
+        function calc_sum_sold(companies) {
+            let sum = 0
+            for(let i=0; i<companies.length; i++) {
+                sum += companies[i].sold;
+            }
+            return sum;
+        }
+
+        function calc_sum_history_rd(companies) {
+            let sum = 0;
+            for(let i=0; i<companies.length; i++) {
+                sum += companies[i].history_rd;
+            }
+            return sum;
+        }
+    }
+}
+
 class Company {
-    constructor(init, decision) {
+    constructor(init) {
         this.init = init;
         this.last = init;
-        this.decision = decision;
+        this.decision = undefined;
     }
+
+    
 
     get prod_rate() {
         return this.decision.prod / this.last.size;
@@ -105,9 +201,11 @@ class Company {
     sum_sales = undefined;
     sum_sold = undefined;
     sum_history_rd = undefined;
+    sum_history_mk = undefined;
     sum_share_effect_price = undefined;
     sum_share_effect_mk = undefined;
     sum_share_effect_rd = undefined;
+    sum_size = undefined;
     
     get sum_mk_compressed() {
         if(this.init.mk*2 < this.average_mk) {
@@ -216,10 +314,95 @@ class Company {
     get spending() {
         return this.prod_cost + this.decision.ci - this,this.depreciation.rd;
     }
+
+    get balance_early() {
+        return this.last.cash - this.last.loan - this.spending;
+    }
+
+    get loan_early() {
+        if(this.balance_early < 0) {
+            return - this.balance_early;
+        }else {
+            return 0;
+        }
+    }
+
+    get interest() {
+        if(this.loan_early == 0) {
+            return this.interest_rate / 8 * this.last.cach;
+        }else {
+            return this.interest_rate / 4 * this.loan_early;
+        }
+    }
+
+    get cost_before_tax() {
+        return this.goods_cost_sold + this.depreciation + this.decision.mk + this.decision.rd - this.interest + this.inventory_charge + 1;
+    }
+
+    get profit_before_tax() {
+        return this.sales - this.cost_before_tax;
+    }
+
+    get balance() {
+        return this.last.cach - this.last.loan + this.loan_early + this.profit - this.decision.ci + this.depreciation + this.goods_cost_sold - this.prod_cost;
+    }
+
+    get loan() {
+        if(0 <= this.balance) {
+            return this.loan_early;
+        }else {
+            return this.loan_early - this.balance;
+        }
+    }
+
+    get cach() {
+        if(0 <= this.balance) {
+            return this.balance;
+        }else {
+            return 0;
+        }
+    }
+
+    get retein() {
+        return this.last.retein + this.profit;
+    }
+
+    get mpi_a() {
+        return 50 * this.retein / this.now_period / this.init.retein;
+    }
+
+    get mpi_b() {
+        return 10 * (this.history_rd + this.history_mk) / (this.sum_history_rd + this.sum_history_mk);
+    }
+
+    get mpi_c() {
+        return 10 * this.size / this.sum_size * this.player_count;
+    }
+
+    get mpi_d() {
+        return 10 * (1 - Math.abs(this.prod_over));
+    }
+
+    get mpi_e() {
+        return 10 * this.sold / this.sum_sold * this.player_count;
+    }
+
+    get mpi_f() {
+        let r = 10 * this.sold / this.last.sold / this.sum_sales * this.last.sum_sales;
+        if(r <= 20) {
+            return r;
+        }else {
+            return 20;
+        }
+    }
+
+    get mpi() {
+        return this.mpi_a + this.mpi_b + this.mpi_c + this.mpi_d + this.mpi_e + this.mpi_f;
+    }
 }
 
 class CompanyReport {
-    constructor(decision, size, capital, employees, inventory, goods_cost_inventory, sum_mk, average_price, history_mk, sum_rd) {
+    constructor(decision, size, capital, employees, inventory, goods_cost_inventory, sum_mk, average_price, history_mk, history_rd, sum_rd, cach, loan, retein, sum_sales) {
         this.decision = decision;
         this.size = size;
         this.capital = capital;
@@ -228,8 +411,13 @@ class CompanyReport {
         this.last.goods_cost_inventory = goods_cost_inventory;
         this.sum_mk = sum_mk;
         this.average_price = average_price;
-        this.history_mk = history_mk:
+        this.history_mk = history_mk;
+        this.history_rd = history_rd;
         this.sum_rd = sum_rd;
+        this.cach = cach;
+        this.loan = loan;
+        this.retein = retein;
+        this.sum_sales = sum_sales;
     }
 }
 
